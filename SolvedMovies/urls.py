99f1d06e8540fa98fa5de0171784464d3ba171a6
JIMMUGUNA789 <jimmuguna@gmail.com>
2022-04-29 23:02:37 +0000

    static files setup

--- SolvedMovies/urls.py
+++ SolvedMovies/urls.py
@@ -1,8 +1,14 @@
 
+from turtle import st
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('Movies.urls')),
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
