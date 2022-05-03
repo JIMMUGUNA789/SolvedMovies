e148437023a2aa7add4b71525a738742eff718a1
JIMMUGUNA789 <jimmuguna@gmail.com>
2022-05-03 20:51:43 +0000

    animations added

--- Movies/views.py
+++ Movies/views.py
@@ -6,12 +6,12 @@ apiKey = settings.API_KEY
 BASE_URL = 'https://api.themoviedb.org/3/movie/'
 def getMovies(request):
     # popular movies
-    items = []
+    
     urlPopular = BASE_URL+'popular'+apiKey
     response1 = requests.get(urlPopular)
     popular = response1.json()
-    print (popular)
-    items.extend(popular.results)
+    # print (popular)
+    
     
 
 
@@ -20,10 +20,30 @@ def getMovies(request):
     response2 = requests.get(urlNowPlaying)
     now_playing = response2.json()
     # print(now_playing)
+
+
+    # upcoming
+    urlUpcoming = BASE_URL+'upcoming'+apiKey
+    response3 = requests.get(urlUpcoming)
+    upcoming = response3.json()
+
+    # animations
+    DISCOVER_BASE_URL ='https://api.themoviedb.org/3/discover/movie'
+    urlAnimations = DISCOVER_BASE_URL+apiKey+'&with_genres=16'
+    response4 = requests.get(urlAnimations)
+    animations = response4.json()
+
+
+
+
+
+
+
     context={
-        "popular":popular,
-        "items":items,
+        "popular":popular,        
         "now_playing":now_playing,
+        "upcoming":upcoming,
+        "animations":animations,
     }
     return render(request, 'index.html', context)
 
