91283316836982d384df7a1bacdadf489766416a
JIMMUGUNA789 <jimmuguna@gmail.com>
2022-05-05 23:21:55 +0000

    trailers and genres

--- Movies/urls.py
+++ Movies/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getMovies, name='getmovies'),
     path('detail/<int:id>/', views.details, name='details'),
+    path('genre/<int:id>/', views.specificCategory, name='genre'),
+    path('trailers/<int:id>/', views.trailer, name='trailers'),
 ]
--- Movies/views.py
+++ Movies/views.py
@@ -1,9 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.conf import settings
 import  requests
 
 apiKey = settings.API_KEY
 BASE_URL = 'https://api.themoviedb.org/3/movie/'
+
+# get all categories
+urlCategories = 'https://api.themoviedb.org/3/genre/movie/list'+apiKey
+responseCategory = requests.get(urlCategories)
+categories = responseCategory.json()
+
+
+
 def getMovies(request):
     # popular movies
     
@@ -44,6 +53,7 @@ def getMovies(request):
         "now_playing":now_playing,
         "upcoming":upcoming,
         "animations":animations,
+        "categories":categories,
     }
     return render(request, 'index.html', context)
 
@@ -58,12 +68,34 @@ def details(request, id):
     urlCast = BASE_URL+strId+'/credits'+apiKey
     response1 = requests.get(urlCast)
     movieCast = response1.json()
-    print(movieCast)
+    # print(movieCast)
     context={
         "movieDetail":movieDetail,
         "movieCast":movieCast,
+        "categories":categories
 
     }
     return render(request, 'detail.html', context)
 
+    #specific category
+def specificCategory(request, id):
+    ID = str(id)
+    url = 'https://api.themoviedb.org/3/discover/movie'+apiKey+'&with_genres='+ID
+    genreData = requests.get(url)
+    genreMovies = genreData.json()
+    context={
+        "genreMovies":genreMovies,
+    }
+    return render(request,'genre.html', context)
+
+
+def trailer(request, id):
+    ID =str(id)
+    url = BASE_URL+'ID'+'/videos'+apiKey
+    trailer = requests.get(url)
+    trailers  = trailer.json()
+    context={
+        "trailers":trailers,
+    }
+    return render(request, 'trailer.html', context)
 
