e6fa0dc9c6462430aa5c002cdbaa7b1184b1b7ce
JIMMUGUNA789 <mugunajim@gmail.com>
2023-06-26 12:01:02 +0000

    host on python anywhere

--- SolvedMovies/settings.py
+++ SolvedMovies/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-56c47*g%m!f(=f9lze97*$y2p%bcm-8a@&2zlkn_1&pk5)-!k9'
+SECRET_KEY = 'django-insecure-56c47*g%m!f(=f9lze97*$y2p%bcm-8a@&2zlkn_1&pk5)-!k9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
